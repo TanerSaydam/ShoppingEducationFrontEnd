9c866df61019d1d90b0ad720575f3f370c9693d3
Taner Saydam <tanersaydam@gmail.com>
2022-05-07 03:40:24 +0300

    .

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,11 +5,12 @@ import { LayoutsComponent } from './components/layouts/layouts.component';
 import { NotFoundComponent } from './components/not-found/not-found.component';
 import { OrderComponent } from './components/order/order.component';
 import { ProductAddComponent } from './components/product/product-add/product-add.component';
+import { AuthGuard } from './guards/auth.guard';
 
 const routes: Routes = [
   { path:'', component: LayoutsComponent, children:[
     { path:'', component:HomeComponent},
-    { path:'product-add',component:ProductAddComponent},
+    { path:'product-add', canActivate:[AuthGuard],component:ProductAddComponent},
     { path: 'orders', component:OrderComponent}
   ]},
   { path: '**', component:NotFoundComponent}
--- src/app/components/layouts/navbar/navbar.component.ts
+++ src/app/components/layouts/navbar/navbar.component.ts
@@ -1,5 +1,6 @@
 import { AfterContentChecked, Component, OnInit } from '@angular/core';
 import { BasketModel } from 'src/app/models/basket';
+import { AuthService } from 'src/app/services/auth.service';
 import { BasketService } from 'src/app/services/basket.service';
 
 @Component({
@@ -11,17 +12,28 @@ export class NavbarComponent implements OnInit, AfterContentChecked {
 
   baskets:BasketModel[] = [];
   total:number = 0;
+  isAuth:boolean = false;
 
   constructor(
-    private basketService:BasketService
+    private basketService:BasketService,
+    private authService:AuthService
   ) { }
 
   ngOnInit(): void {
     this.baskets = this.basketService.baskets;
+    this.isAuth = this.authService.isAuth;
   }
 
   ngAfterContentChecked(): void {
     this.total = this.basketService.total;
+    this.isAuth = this.authService.isAuth;
   }
 
+  login(){
+    this.authService.login();
+  }
+
+  logout(){
+    this.authService.logout();
+  }
 }
